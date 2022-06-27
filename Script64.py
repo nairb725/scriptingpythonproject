0d409732c4368a0b12d89f2eda31ed5043891012
Infernaton <lubin.pauly@gmail.com>
2022-06-27 14:22:06 +0200

    toBase64

--- Script64.py
+++ Script64.py
@@ -2,6 +2,24 @@ def split(str):
     return [ord(char) for char in str]
 
 
+"""
+Transform the octet list to a base64 friendly list
+"""
+def toBase64(arr):
+    return_arr = []
+    str = ""  # the global string
+    n = 6  # the index to cut the string for the base64
+
+    for i in arr:
+        str += i
+
+    # split the whole string into string of 6 character each
+    for index in range(0, len(str), n):
+        return_arr.append(str[index: index + n])
+
+    return return_arr
+
+
 # Taking an input values from user
 values = input("Enter the Values: ")
 
