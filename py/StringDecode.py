c8b75d3111d6abf28953b6c6c3addab69e47cb40
Brian <nairb725@gmail.com>
2022-06-28 15:41:29 +0200

    Update StringDecode.py

--- py/StringDecode.py
+++ py/StringDecode.py
@@ -131,7 +131,7 @@ def ascii(arr):
     Returns: array with ascii numbers
 
     """
-    return
+    return arr.decode()
 
 
 def alphabet(arr):
@@ -143,7 +143,7 @@ def alphabet(arr):
     Returns: array with alphabet letters
 
     """
-    return [chr(char - 65) for char in arr]
+    return [(ord(char) - 65) for char in arr]
 
 
 def byte(str):
