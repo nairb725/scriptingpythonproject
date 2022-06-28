a1f74594d9652826484847131668584e64b18585
progresso85 <martin.heuripeau@gmail.com>
2022-06-28 16:25:22 +0200

    add Argparse
    Base64 + encode/decode

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from py.StringDecode import *
 from py.StringEncode import *
+import argparse
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+parser.add_argument("Base64")
+parser.add_subparsers("encode", "decode")
+#group.add_argument("-S", "-f")
+
+
+
+# subparsers = parser.add_subparsers("encode", "decode")
 
 # Taking an input values from user
-values = input("Enter the Values: ")
+# values = input("Enter the Values: ")
 
-print(to_base64(values))
+# print(to_base64(values))
