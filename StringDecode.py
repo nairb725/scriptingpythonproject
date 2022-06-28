bbc7ec8ae89fd3eb129f05fcde327aeaafa49e89
progresso85 <martin.heuripeau@gmail.com>
2022-06-28 13:54:06 +0200

    3 def
    def alphabet , ascii et binary

---
+++ StringDecode.py
@@ -0,0 +1,38 @@
+
+def binary():
+    """
+            decode a number to binary mode
+        Args:
+            number: int
+
+        Returns: binary
+
+        """
+    return
+
+
+
+def ascii():
+
+    """
+            decode a binary to ascii
+        Args:
+            number: int
+
+        Returns: ascii
+
+        """
+    return
+
+def alphabet():
+
+    """
+            decode a ascii to alphabet
+        Args:
+            number: char
+
+        Returns: alpahbet
+
+        """
+    return
+
