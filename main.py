1d2a7bcdffb31c19b91e248048b0c613130a214a
progresso85 <martin.heuripeau@gmail.com>
2022-06-29 14:01:49 +0200

    add SriptAdmin

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from py.StringEncode import *
 import argparse
 
 parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group()
+# group = parser.add_mutually_exclusive_group()
 parser.add_argument("Base64")
-parser.add_subparsers("encode", "decode")
-#group.add_argument("-S", "-f")
+# parser.add_subparsers("encode", "decode")
+# group.add_argument("-S", "-f")
 
-
+# Parse and print the results
+args = parser.parse_args()
+print(args)
 
 # subparsers = parser.add_subparsers("encode", "decode")
 
