ff04a338299ba721a3398cbd03c8bee473e61aaf
Brian <nairb725@gmail.com>
2022-07-01 11:05:27 +0200

    link to data base V1

--- scriptingadmin/ScriptAdmin.py
+++ scriptingadmin/ScriptAdmin.py
@@ -1,8 +1,32 @@
 import psutil
+import json
+# from influxdb import InfluxDBClient
+from datetime import datetime
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# You can generate an API token from the "API Tokens Tab" in the UI
+token = "pVgJdDDf3MQGikSdaoBfjCovDRR_TcPoUxNW11vmzftXtG3WV5sx5o473gpXam8mDroFqE888dUs0wHDl-qOBQ=="
+org = "b6548341542107d5"
+bucket = "scripting"
 
 
 def metrics():
-    print(psutil.cpu_percent())
-    print(psutil.net_io_counters())
-    print(psutil.sensors_battery(), "platform not supported")
-    print('memory used:', psutil.virtual_memory()[2], '%')
+    """
+      link to influx bdd/create databse/get computer information§right it down on the bdd and stop the program
+    """
+
+    with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        data = {
+            "measurement": "measure",
+            "fields": {
+                "cpu_percent": psutil.cpu_percent(),
+                'net': psutil.net_io_counters().packets_sent,
+                'power': psutil.sensors_battery().percent,
+                "ram": psutil.virtual_memory()[2]
+            }
+        }
+        print(data)
+        write_api.write(bucket, org, data)
+
